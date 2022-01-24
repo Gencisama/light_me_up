59e8cd70a5b9e0862ee6e527ae14b90b68e196ec
Gencisama <39381217+Gencisama@users.noreply.github.com>
2022-01-24 10:38:17 +0100

    Add files via upload

---
+++ webinar.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import time
+
+st.header('Webinar')
+st.subheader('DI Genc Islamaj')
+st.text('')
+st.subheader('This is a website to tell the world why Edona Islamaj is the best')
+
+st.text('To find the truth please continue down below')
+
+if st.button('Find out'):
+    st.text('calculating data....')
+    time.sleep(4)
+    st.text('could not find the answer on planet earth....')
+    time.sleep(2)
+    st.text('looking for data in planet earth C345F')
+    time.sleep(4)
+    st.text('could not find the answer on planet earth C345F....')
+    time.sleep(2)
+    st.text('looking for data in universe ....')
+    time.sleep(4)
+    st.text('could not find an answer...')
+    time.sleep(2)
+    st.text('looking for data in the multiverse ....')
+    st.text('please hold on this might take a bit ...')
+    time.sleep(5)
+    st.text('Answer found!')
+    st.text('...')
+    time.sleep(3)
+    st.text('its because of this mf')
+    st.image('./me.png')
